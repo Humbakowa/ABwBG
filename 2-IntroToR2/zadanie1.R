196eb49afcc9b579049cf4ba4a2db282c0e0e0d9
Humbakowa <katarzyna.ewa.kaca@gmail.com>
2024-10-15 11:00:17 +0200

    Zadanie 1- Humbakowa
    Dodanie komentarzy

---
+++ 2-IntroToR2/zadanie1.R
@@ -0,0 +1,26 @@
+## Zadanie 1: Zapoznanie się z RStudio
+
+**Cel:** Poznanie interfejsu i podstawowych funkcji RStudio.
+
+### Instrukcje i rozwiązanie:
+
+1.  **Uruchom RStudio** na swoim komputerze.
+
+2.  **Zapoznaj się z interfejsem użytkownika:**
+  
+  3.  **Stwórz nowy skrypt R:**
+  
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+#---------------Funkcje paneli---------------
+#Source (edytor skryptu) służy do edycji skryptu, a funkcje nie są automatycznie wykonywane
+#Console służy do wykonywania skryptów i zadań
+#Środowisko (environment) służy do podejrzenia zdefiniowanych wartości
+#panel wyjściowy służy do ułatwienia importowania własnych danych, tworzenia plików, ustalania gdzie będą pliki i ścieżki do zapisu
+
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+#---------------Motyw kolorystyczny---------------
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+#czarne tło, żeby nie bolały oczy i fioletowy z fuksją, bo lubię. Granatowe komentarze też są ładne
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
